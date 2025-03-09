653eac759bfbc30bfd5f278cf3eee367eb0f680b
idncod <idncod@protonmail.com>
2025-03-09 23:19:40 +0000

    add app compomemt with static table

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css';
+import React, { useState } from "react";
+import UserTable from "./components/users/UserTable/UserTable";
 
-const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
+interface User {
+    uuid: string;
+    name: string;
+    surname: string;
+    email: string;
+}
+
+const App: React.FC = () => {
+    const [users, setUsers] = useState<User[]>([
+        { uuid: "123e4567-e89b-12d3-a456-426614174000", name: "John", surname: "Doe", email: "john.doe@example.com" },
+        { uuid: "123e4567-e89b-12d3-a456-426614174001", name: "Jane", surname: "Doe", email: "jane.doe@example.com" },
+    ]);
+
+    return (
+        <div className="container">
+            <div className="card">
+                <UserTable users={users} />
+            </div>
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/users/UserTable/UserTable.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+interface User {
+    uuid: string;
+    name: string;
+    surname: string;
+    email: string;
+}
+
+interface TableProps {
+    users: User[];
+}
+
+const UserTable: React.FC<TableProps> = ({ users }) => {
+    return (
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>UUID</th>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                {users.map((user) => (
+                    <tr key={user.uuid}>
+                        <td>{user.uuid}</td>
+                        <td>{user.name}</td>
+                        <td>{user.surname}</td>
+                        <td>{user.email}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default UserTable;
