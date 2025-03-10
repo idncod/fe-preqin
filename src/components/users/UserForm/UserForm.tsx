e25f1e22c93e15f8745ebf6d89b0d5fa3884d78c
idncod <idncod@protonmail.com>
2025-03-10 00:47:37 +0000

    add tweaks to styling of the form and its container

--- src/components/users/UserForm/UserForm.tsx
+++ src/components/users/UserForm/UserForm.tsx
@@ -17,28 +17,28 @@ const UserForm: React.FC = () => {
     return (
         <div className={styles.formContainer}>
             <form onSubmit={handleSubmit}>
-                <div>
+                <div className={styles.field}>
                     <label>UUID:</label>
                     <input type="text" value={uuid} onChange={(e) => setUuid(e.target.value)} required
                            disabled />
                 </div>
-                <div>
+                <div className={styles.field}>
                     <label>Name:</label>
                     <input type="text" value={name} onChange={(e) => setName(e.target.value)} required/>
                 </div>
-                <div>
+                <div className={styles.field}>
                     <label>Surname:</label>
                     <input type="text" value={surname} onChange={(e) => setSurname(e.target.value)} required/>
                 </div>
-                <div>
+                <div className={styles.field}>
                     <label>Email:</label>
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                 </div>
-                <div>
+                <div className={styles.field}>
                     <label>Company:</label>
                     <input type="text" value={company} onChange={(e) => setCompany(e.target.value)} required/>
                 </div>
-                <div>
+                <div className={styles.field}>
                     <label>Job Title:</label>
                     <input type="text" value={jobTitle} onChange={(e) => setJobTitle(e.target.value)} required/>
                 </div>
